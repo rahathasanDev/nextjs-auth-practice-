6aad412f88065cce74d98d6fc1f6b3bbd9dadb1e
rahathasanDev <devrahat.hasan@gmail.com>
2024-05-28 20:20:55 +0600

    Register User Using Next.Js Server Actions And A Custom Server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ const HomePage = () => {
     <div>
       <h1 className="text-4xl text-center mt-10">Welcome To Home Page</h1>
     </div>
+    
+    
+
   );
 };
 
---
+++ src/utils/actions/registerUser.ts
@@ -0,0 +1,17 @@
+"use server";
+
+
+export const registerUser = async(data) =>{
+  const  res = await fetch(`${process.env.BACKEND_URL}/register`,{
+    method:'POST',
+    headers:{
+      "Content-Type":"application/json",
+    },
+    body:JSON.stringify(data),
+    cache:"no-store"
+  });
+  const userInfo = await res.json();
+
+
+  return userInfo;
+}
